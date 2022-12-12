4753e77a9703778b573b14c9d204552189f774ff
Tamirlan <omarovt96@gmail.com>
2022-12-12 19:59:23 +0600

    Implement Run for running parallel tasks

---
+++ parallel/parallel.go
@@ -0,0 +1,88 @@
+package parallel
+
+import "errors"
+
+var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
+
+type Task func() error
+
+// worker gets task from tasks channel, runs it and puts result into results channel
+// When it receives message from quit channel, it stops.
+func worker(tasks <-chan Task, quit <-chan bool, results chan<- error) {
+	for {
+		select {
+		case task := <-tasks:
+			results <- task()
+		case <-quit:
+			return
+		}
+	}
+}
+
+// createTasksChannel returns unbuffered channel which contains tasks
+func createTasksChannel(tasks []Task) chan Task {
+	tasksChannel := make(chan Task)
+
+	go func() {
+		for _, task := range tasks {
+			tasksChannel <- task
+		}
+	}()
+
+	return tasksChannel
+}
+
+// runTasks runs n worker goroutines and returns two channels.
+// First is a channel for consuming results.
+// Second is a channel for stopping worker goroutines.
+func runTasks(n int, tasksChannel chan Task) (<-chan error, chan bool) {
+	resultsChannel := make(chan error)
+	quitChannel := make(chan bool)
+
+	for i := 0; i < n; i++ {
+		go worker(tasksChannel, quitChannel, resultsChannel)
+	}
+
+	return resultsChannel, quitChannel
+}
+
+// checkTaskResults consumes results from resultsChannel taskCount times and
+// checks for errors. If number of errors exceedes errorLimit, corresponding error is returned.
+func checkTaskResults(
+	taskCount int,
+	errorLimit int,
+	resultsChannel <-chan error,
+	quitChannel chan<- bool,
+) error {
+
+	// Stop all worker goroutines after returning from this function
+	defer func() {
+		quitChannel <- true
+	}()
+
+	if errorLimit < 0 {
+		errorLimit = 0
+	}
+
+	for i := 0; i < taskCount; i++ {
+		result := <-resultsChannel
+
+		if result != nil {
+			errorLimit--
+		}
+
+		if errorLimit < 0 {
+			return ErrErrorsLimitExceeded
+		}
+	}
+
+	return nil
+}
+
+// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks
+func Run(tasks []Task, n, m int) error {
+	tasksChannel := createTasksChannel(tasks)
+	resultsChannel, quitChannel := runTasks(n, tasksChannel)
+
+	return checkTaskResults(len(tasks), m, resultsChannel, quitChannel)
+}
---
+++ parallel/parallel_test.go
@@ -0,0 +1,97 @@
+package parallel
+
+import (
+	"errors"
+	"math/rand"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func createTask(durationSeconds int) Task {
+	return func() error {
+		time.Sleep(time.Duration(durationSeconds) * time.Second)
+		return nil
+	}
+}
+
+func createTaskWithError(durationSeconds int) Task {
+	return func() error {
+		time.Sleep(time.Duration(durationSeconds) * time.Second)
+		return errors.New("Error during calculation")
+	}
+}
+
+func TestRunWithMultipleTasks(t *testing.T) {
+	var tasks []Task
+
+	for i := 0; i < 1000; i++ {
+		tasks = append(tasks, createTask(rand.Intn(5)))
+	}
+
+	assert.Nil(t, Run(tasks, 100, 0))
+}
+
+
+func TestRunWhenTasksLessThanGoroutines(t *testing.T) {
+	var tasks []Task
+
+	for i := 0; i < 100; i++ {
+		tasks = append(tasks, createTask(rand.Intn(5)))
+	}
+
+	assert.Nil(t, Run(tasks, 1000, 0))
+}
+
+func TestRunMultipleTasksWithErrors(t *testing.T) {
+	var tasks []Task
+
+	for i := 0; i < 100; i++ {
+		tasks = append(tasks, createTask(rand.Intn(5)))
+	}
+
+	for i := 0; i < 50; i++ {
+		tasks = append(tasks, createTaskWithError(rand.Intn(5)))
+	}
+
+	assert.Nil(t, Run(tasks, 100, 100))
+}
+
+func TestRunMultipleTasksWithTooManyErrors(t *testing.T) {
+	var tasks []Task
+
+	for i := 0; i < 100; i++ {
+		tasks = append(tasks, createTask(rand.Intn(5)))
+	}
+
+	for i := 0; i < 50; i++ {
+		tasks = append(tasks, createTaskWithError(rand.Intn(5)))
+	}
+
+	assert.Equal(t, Run(tasks, 100, 10), ErrErrorsLimitExceeded)
+}
+
+func TestRunWhenErrorsCountZero(t *testing.T) {
+	var tasks []Task
+
+	for i := 0; i < 100; i++ {
+		tasks = append(tasks, createTask(rand.Intn(5)))
+	}
+
+	tasks = append(tasks, createTaskWithError(rand.Intn(5)))
+
+	assert.Equal(t, Run(tasks, 100, 0), ErrErrorsLimitExceeded)
+}
+
+func TestRunWhenErrorsCountNegative(t *testing.T) {
+	var tasks []Task
+
+	for i := 0; i < 100; i++ {
+		tasks = append(tasks, createTask(rand.Intn(5)))
+	}
+
+	tasks = append(tasks, createTaskWithError(rand.Intn(5)))
+
+	assert.Equal(t, Run(tasks, 100, -12), ErrErrorsLimitExceeded)
+}
