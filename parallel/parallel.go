4b493f34b50a6ae39be45f65b6eccea6ed718085
Tamirlan Omarov <omarovt96@gmail.com>
2022-12-30 01:57:54 +0600

    Simplify solution

--- parallel/parallel.go
+++ parallel/parallel.go
@@ -1,6 +1,9 @@
 package parallel
 
-import "errors"
+import (
+	"errors"
+	"sync"
+)
 
 var ErrErrorsLimitExceeded = errors.New("errors limit exceeded")
 
@@ -8,80 +11,59 @@ type Task func() error
 
 // worker gets task from tasks channel, runs it and puts result into results channel
 // When it receives message from quit channel, it stops.
-func worker(tasks <-chan Task, quit <-chan struct{}, results chan<- error) {
-	for {
-		select {
-		case task := <-tasks:
-			results <- task()
-		case <-quit:
-			return
-		}
-	}
-}
+func Run(tasks []Task, n int, m int) error {
+	tasksCh := make(chan func() error)
+	resultsCh := make(chan error)
+	quitCh := make(chan struct{})
+	wg := sync.WaitGroup{}
 
-// createTasksChannel returns unbuffered channel which contains tasks.
-func createTasksChannel(tasks []Task) chan Task {
-	tasksChannel := make(chan Task)
+	if m < 0 {
+		m = 0
+	}
 
 	go func() {
+		defer close(tasksCh)
 		for _, task := range tasks {
-			tasksChannel <- task
+			select {
+			case tasksCh <- task:
+			case <-quitCh:
+				return
+			}
 		}
 	}()
 
-	return tasksChannel
-}
-
-// runTasks runs n worker goroutines and returns two channels.
-// First is a channel for consuming results.
-// Second is a channel for stopping worker goroutines.
-func runTasks(n int, tasksChannel chan Task) (<-chan error, chan struct{}) {
-	resultsChannel := make(chan error)
-	quitChannel := make(chan struct{})
-
+	wg.Add(n)
 	for i := 0; i < n; i++ {
-		go worker(tasksChannel, quitChannel, resultsChannel)
+		go func() {
+			defer wg.Done()
+			for task := range tasksCh {
+				select {
+				case resultsCh <- task():
+				case <-quitCh:
+					return
+				}
+			}
+		}()
 	}
 
-	return resultsChannel, quitChannel
-}
+	go func() {
+		for result := range resultsCh {
+			if result != nil {
+				m--
 
-// checkTaskResults consumes results from resultsChannel taskCount times and
-// checks for errors. If number of errors exceedes errorLimit, corresponding error is returned.
-func checkTaskResults(
-	taskCount int,
-	errorLimit int,
-	resultsChannel <-chan error,
-	quitChannel chan<- struct{},
-) error {
-	// Stop all worker goroutines after returning from this function
-	defer func() {
-		quitChannel <- struct{}{}
+				if m < 0 {
+					close(quitCh)
+					return
+				}
+			}
+		}
 	}()
 
-	if errorLimit < 0 {
-		errorLimit = 0
-	}
+	wg.Wait()
 
-	for i := 0; i < taskCount; i++ {
-		result := <-resultsChannel
-
-		if result != nil {
-			errorLimit--
-		}
-
-		if errorLimit < 0 {
-			return ErrErrorsLimitExceeded
-		}
+	if m < 0 {
+		return ErrErrorsLimitExceeded
 	}
 
 	return nil
-}
-
-// Run starts tasks in n goroutines and stops its work when receiving m errors from tasks.
-func Run(tasks []Task, n, m int) error {
-	tasksChannel := createTasksChannel(tasks)
-	resultsChannel, quitChannel := runTasks(n, tasksChannel)
-
-	return checkTaskResults(len(tasks), m, resultsChannel, quitChannel)
 }
--- parallel/parallel_test.go
+++ parallel/parallel_test.go
@@ -9,6 +9,8 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+const MaxSleepTime = 3
+
 func createTask(durationSeconds int) Task {
 	return func() error {
 		time.Sleep(time.Duration(durationSeconds) * time.Second)
@@ -26,18 +28,18 @@ func createTaskWithError(durationSeconds int) Task {
 func TestRunWithMultipleTasks(t *testing.T) {
 	var tasks []Task
 
-	for i := 0; i < 1000; i++ {
-		tasks = append(tasks, createTask(rand.Intn(5)))
+	for i := 0; i < 10; i++ {
+		tasks = append(tasks, createTask(rand.Intn(MaxSleepTime)))
 	}
 
-	assert.Nil(t, Run(tasks, 100, 0))
+	assert.Nil(t, Run(tasks, 10, 0))
 }
 
 func TestRunWhenTasksLessThanGoroutines(t *testing.T) {
 	var tasks []Task
 
 	for i := 0; i < 100; i++ {
-		tasks = append(tasks, createTask(rand.Intn(5)))
+		tasks = append(tasks, createTask(rand.Intn(MaxSleepTime)))
 	}
 
 	assert.Nil(t, Run(tasks, 1000, 0))
@@ -47,11 +49,11 @@ func TestRunMultipleTasksWithErrors(t *testing.T) {
 	var tasks []Task
 
 	for i := 0; i < 100; i++ {
-		tasks = append(tasks, createTask(rand.Intn(5)))
+		tasks = append(tasks, createTask(rand.Intn(MaxSleepTime)))
 	}
 
 	for i := 0; i < 50; i++ {
-		tasks = append(tasks, createTaskWithError(rand.Intn(5)))
+		tasks = append(tasks, createTaskWithError(rand.Intn(MaxSleepTime)))
 	}
 
 	assert.Nil(t, Run(tasks, 100, 100))
@@ -61,11 +63,11 @@ func TestRunMultipleTasksWithTooManyErrors(t *testing.T) {
 	var tasks []Task
 
 	for i := 0; i < 100; i++ {
-		tasks = append(tasks, createTask(rand.Intn(5)))
+		tasks = append(tasks, createTask(rand.Intn(MaxSleepTime)))
 	}
 
 	for i := 0; i < 50; i++ {
-		tasks = append(tasks, createTaskWithError(rand.Intn(5)))
+		tasks = append(tasks, createTaskWithError(rand.Intn(MaxSleepTime)))
 	}
 
 	assert.Equal(t, Run(tasks, 100, 10), ErrErrorsLimitExceeded)
@@ -75,10 +77,10 @@ func TestRunWhenErrorsCountZero(t *testing.T) {
 	var tasks []Task
 
 	for i := 0; i < 100; i++ {
-		tasks = append(tasks, createTask(rand.Intn(5)))
+		tasks = append(tasks, createTask(rand.Intn(MaxSleepTime)))
 	}
 
-	tasks = append(tasks, createTaskWithError(rand.Intn(5)))
+	tasks = append(tasks, createTaskWithError(rand.Intn(MaxSleepTime)))
 
 	assert.Equal(t, Run(tasks, 100, 0), ErrErrorsLimitExceeded)
 }
@@ -87,10 +89,10 @@ func TestRunWhenErrorsCountNegative(t *testing.T) {
 	var tasks []Task
 
 	for i := 0; i < 100; i++ {
-		tasks = append(tasks, createTask(rand.Intn(5)))
+		tasks = append(tasks, createTask(rand.Intn(MaxSleepTime)))
 	}
 
-	tasks = append(tasks, createTaskWithError(rand.Intn(5)))
+	tasks = append(tasks, createTaskWithError(rand.Intn(MaxSleepTime)))
 
 	assert.Equal(t, Run(tasks, 100, -12), ErrErrorsLimitExceeded)
 }
@@ -100,7 +102,7 @@ func TestRunEnsureTasksRunningConcurrently(t *testing.T) {
 	var totalTasksDuration time.Duration
 
 	for i := 0; i < 100; i++ {
-		duration := rand.Intn(5)
+		duration := rand.Intn(MaxSleepTime)
 		tasks = append(tasks, createTask(duration))
 		totalTasksDuration += (time.Duration(duration) * time.Second)
 	}
